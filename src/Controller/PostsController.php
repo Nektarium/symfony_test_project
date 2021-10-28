5edf850bf504fa31d4dfeffe626f9be9fa42102b
Nektarium <shmorosoff@gmail.com>
2021-10-29 02:06:28 +0300

    first model and migration

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,13 @@ class PostsController extends AbstractController
 {
     #[Route('/posts', name: 'posts')]
     public function index(): Response
-    {
+    {   
+        //$posts = [];
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+        
         return $this->render('posts/index.html.twig', [
             'controller_name' => 'PostsController',
+            'posts' => $posts,
         ]);
     }
 }
